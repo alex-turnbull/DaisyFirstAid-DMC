9ed60b0f7b295c2714cd64041cc41010c67bc78f
alex-turnbull <alexturnbull1@virginmedia.com>
2019-10-10 15:49:48 +0100

    Added unity project

---
+++ Unity/DaisyFirstAid/Assets/Scripts/UIControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIControl : MonoBehaviour
+{
+    public Text colourThing;
+    GraphicRaycaster raycaster;
+
+    void Awake()
+    {
+        // Get both of the components we need to do this
+        raycaster = GetComponent<GraphicRaycaster>();
+    }
+
+    void Update()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0) || touch.phase == TouchPhase.Began)
+            {
+                //Set up the new Pointer Event
+                PointerEventData pointerData = new PointerEventData(EventSystem.current);
+                List<RaycastResult> results = new List<RaycastResult>();
+
+                //Raycast using the Graphics Raycaster and mouse click position
+                pointerData.position = Input.GetTouch(0).position;
+                raycaster.Raycast(pointerData, results);
+
+                //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
+                foreach (RaycastResult result in results)
+                {
+                    colourThing.text = $"HIT: {result.gameObject.name}";
+                }
+            }
+        }
+    }
+}
