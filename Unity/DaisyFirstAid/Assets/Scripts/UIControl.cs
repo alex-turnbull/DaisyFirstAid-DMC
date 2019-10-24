c39bed74c3b424bfe7f370556a53ccb385ec1ee2
alex-turnbull <alexturnbull1@virginmedia.com>
2019-10-24 17:56:40 +0100

    full implemented page segments, visual updates, control etc

--- Unity/DaisyFirstAid/Assets/Scripts/UIControl.cs
+++ Unity/DaisyFirstAid/Assets/Scripts/UIControl.cs
@@ -27,6 +27,9 @@ public class UIControl : MonoBehaviour
 
     private bool moving = false;
 
+    public Image returnButtonImg;
+    public Sprite[] returnImages;
+
     void Awake()
     {
         raycaster = GetComponent<GraphicRaycaster>();
@@ -44,143 +47,164 @@ public class UIControl : MonoBehaviour
 
     void Update()
     {
-        foreach (Touch touch in Input.touches)
+        if(currentPage.name != "MENU PAGE")
         {
-            if (Input.GetKeyDown(KeyCode.Mouse0) || touch.phase == TouchPhase.Ended)
-            {
-                //Set up the new Pointer Event
-                PointerEventData pointerData = new PointerEventData(EventSystem.current);
-                List<RaycastResult> results = new List<RaycastResult>();
-
-                //Raycast using the Graphics Raycaster and mouse click position
-                pointerData.position = Input.GetTouch(0).position;
-                raycaster.Raycast(pointerData, results);
-
-                moving = pointerData.IsScrolling();
-                //if (good = true) work; 
-
-
-                //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
-                foreach (RaycastResult result in results)
-                {
-                    //if (result.gameObject.name == "MenuButton")
-                    //{
-                    //    StopAllCoroutines();
-                    //    coroutine = openMenu();
-                    //    StartCoroutine(coroutine);
-                    //}
-
-                    //if (result.gameObject.name == "Return" || results[0].gameObject.name == "menuBack")
-                    //{
-                    //    StopAllCoroutines();
-                    //    coroutine = closeMenu();
-                    //    StartCoroutine(coroutine);
-                    //}
-
-                    //if (result.gameObject.name == "ReturnButton")
-                    //{
-                    //    previousPage();
-                    //}
-
-                    //if (result.gameObject.tag == "PageButton")
-                    //{
-                    //    if (result.gameObject.GetComponent<pageToOpen>() != null)
-                    //    {
-                   //         changePage(result.gameObject.GetComponent<pageToOpen>().pageRef);
-                    //    }
-                    //}
-
-                    colourThing.text = $"HIT: {result.gameObject.name}";
-                }              
-            }
+            returnButtonImg.sprite = returnImages[1];
         }
+        else
+        {
+            returnButtonImg.sprite = returnImages[0];
+        }
+
+        ///OLD RAYCASTING CODE FOR BUTTONS, ALL DONE THROUGH EVENTS NOW LIKE A GOOD BOY
+        ///
+        //foreach (Touch touch in Input.touches)
+        //{
+        //    if (Input.GetKeyDown(KeyCode.Mouse0) || touch.phase == TouchPhase.Ended)
+        //    {
+        //        //Set up the new Pointer Event
+        //        PointerEventData pointerData = new PointerEventData(EventSystem.current);
+        //        List<RaycastResult> results = new List<RaycastResult>();
+
+        //        //Raycast using the Graphics Raycaster and mouse click position
+        //        pointerData.position = Input.GetTouch(0).position;
+        //        raycaster.Raycast(pointerData, results);
+
+        //        moving = pointerData.IsScrolling();
+        //        //if (good = true) work; 
+
+
+        //        //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
+        //        foreach (RaycastResult result in results)
+        //        {
+        //            //if (result.gameObject.name == "MenuButton")
+        //            //{
+        //            //    StopAllCoroutines();
+        //            //    coroutine = openMenu();
+        //            //    StartCoroutine(coroutine);
+        //            //}
+
+        //            //if (result.gameObject.name == "Return" || results[0].gameObject.name == "menuBack")
+        //            //{
+        //            //    StopAllCoroutines();
+        //            //    coroutine = closeMenu();
+        //            //    StartCoroutine(coroutine);
+        //            //}
+
+        //            //if (result.gameObject.name == "ReturnButton")
+        //            //{
+        //            //    previousPage();
+        //            //}
+
+        //            //if (result.gameObject.tag == "PageButton")
+        //            //{
+        //            //    if (result.gameObject.GetComponent<pageToOpen>() != null)
+        //            //    {
+        //           //         changePage(result.gameObject.GetComponent<pageToOpen>().pageRef);
+        //            //    }
+        //            //}
+
+        //            colourThing.text = $"HIT: {result.gameObject.name}";
+        //        }              
+        //    }
+        //}
 
 
         //--------------------------------DEBUGGING UNITY--------------------------------
-        if (Input.GetKeyDown("o"))
-        {
-            StopAllCoroutines();
-            coroutine = openMenu();
-            StartCoroutine(coroutine);
-        }
-        if (Input.GetKeyDown("p"))
-        {
-            StopAllCoroutines();
-            coroutine = closeMenu();
-            StartCoroutine(coroutine);
-        }        
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            changePage(pagesHolder.transform.GetChild(1).gameObject);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            changePage(pagesHolder.transform.GetChild(2).gameObject);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            changePage(pagesHolder.transform.GetChild(3).gameObject);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            changePage(pagesHolder.transform.GetChild(4).gameObject);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            changePage(pagesHolder.transform.GetChild(5).gameObject);
-        }
-        if (Input.GetKeyDown(KeyCode.Backspace))
-        {
-            previousPage();
-        }
+        //if (Input.GetKeyDown("o"))
+        //{
+        //    StopAllCoroutines();
+        //    coroutine = openMenu();
+        //    StartCoroutine(coroutine);
+        //}
+        //if (Input.GetKeyDown("p"))
+        //{
+        //    StopAllCoroutines();
+        //    coroutine = closeMenu();
+        //    StartCoroutine(coroutine);
+        //}        
+        //if (Input.GetKeyDown(KeyCode.Alpha1))
+        //{
+        //    changePage(pagesHolder.transform.GetChild(1).gameObject);
+        //}
+        //if (Input.GetKeyDown(KeyCode.Alpha2))
+        //{
+        //    changePage(pagesHolder.transform.GetChild(2).gameObject);
+        //}
+        //if (Input.GetKeyDown(KeyCode.Alpha3))
+        //{
+        //    changePage(pagesHolder.transform.GetChild(3).gameObject);
+        //}
+        //if (Input.GetKeyDown(KeyCode.Alpha4))
+        //{
+        //    changePage(pagesHolder.transform.GetChild(4).gameObject);
+        //}
+        //if (Input.GetKeyDown(KeyCode.Alpha5))
+        //{
+        //    changePage(pagesHolder.transform.GetChild(5).gameObject);
+        //}
+        //if (Input.GetKeyDown(KeyCode.Backspace))
+        //{
+        //    previousPage();
+        //}
         //--------------------------------------------------------------------------------
 
         //ANDROID BACK BUTTON
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            previousPage();
+            if (menuOpen)
+            {
+                btn_closeMenu();
+            }
+            else
+            {
+                previousPage();
+            }
+            
         }
     }
 
     IEnumerator openMenu()
     {
+        var EE = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
         menuOpen = true;
         menuBack.SetActive(true);
         float elapsedTime = 0;
         float waitTime = 3f;
         RectTransform menuTrans = menu.GetComponent<RectTransform>();
 
-        while (Vector3.Distance(menuTrans.anchoredPosition, new Vector3(0,210,0)) > 0.2)
+        while (Vector3.Distance(menuTrans.anchoredPosition, new Vector3(0, menuTrans.anchoredPosition.y, 0)) > 0.2)
         {
-            menuTrans.anchoredPosition = Vector3.Lerp(menuTrans.anchoredPosition, new Vector3(0,210,0), (elapsedTime / waitTime));
+            menuTrans.anchoredPosition = Vector3.Lerp(menuTrans.anchoredPosition, new Vector3(0, menuTrans.anchoredPosition.y, 0), (elapsedTime / waitTime));
             elapsedTime += Time.deltaTime;
 
             // Yield here
             yield return null;
         }
         // Make sure we got there
-        menuTrans.anchoredPosition = new Vector3(0, 210, 0);
+        menuTrans.anchoredPosition = new Vector3(0, menuTrans.anchoredPosition.y, 0);
         yield return null;
     }
 
     IEnumerator closeMenu()
     {
+        var EE = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
         menuOpen = false;
         menuBack.SetActive(false);
         float elapsedTime = 0;
         float waitTime = 3f;
         RectTransform menuTrans = menu.GetComponent<RectTransform>();
 
-        while (Vector3.Distance(menuTrans.anchoredPosition, new Vector3(-540, 210, 0)) > 0.2)
+        while (Vector3.Distance(menuTrans.anchoredPosition, new Vector3(-540, menuTrans.anchoredPosition.y, 0)) > 0.2)
         {
-            menuTrans.anchoredPosition = Vector3.Lerp(menuTrans.anchoredPosition, new Vector3(-540, 210, 0), (elapsedTime / waitTime));
+            menuTrans.anchoredPosition = Vector3.Lerp(menuTrans.anchoredPosition, new Vector3(-540, menuTrans.anchoredPosition.y, 0), (elapsedTime / waitTime));
             elapsedTime += Time.deltaTime;
 
             // Yield here
             yield return null;
         }
         // Make sure we got there
-        menuTrans.anchoredPosition = new Vector3(-540, 210, 0);
+        menuTrans.anchoredPosition = new Vector3(-540, menuTrans.anchoredPosition.y, 0);
         yield return null;
     }
 
@@ -188,10 +212,13 @@ public class UIControl : MonoBehaviour
     {
         if (!moving)
         {
-            pageHistory.Add(currentPage);
-            currentPage.SetActive(false);
-            currentPage = pageObject;
-            currentPage.SetActive(true);
+            if(pageObject != null)
+            {
+                pageHistory.Add(currentPage);
+                currentPage.SetActive(false);
+                currentPage = pageObject;
+                currentPage.SetActive(true);
+            }            
         }
         
 
